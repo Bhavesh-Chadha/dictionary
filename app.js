ba1766acf8976f5a5850c8dc9ab198c1cf591ebb
Bhavesh-Chadha <bhaveshchadha14@gmail.com>
2020-12-08 23:22:23 +0530

    basic design done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 wordSearch = document.querySelector("#txt-area")
-outputDiv = document.querySelector("#output-div")
+inputDiv1 = document.querySelector("#input-div1")
+inputDiv2 = document.querySelector("#input-div2")
+inputDiv3 = document.querySelector("#input-div3")
+inputDiv4 = document.querySelector("#input-div4")
 searchButton = document.querySelector("#searchButton")
 var word = wordSearch.value
 a = "word"
@@ -14,16 +17,18 @@ function search() {
     url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
     url = url + wordSearch.value
 
-    console.log("reached", wordSearch.value, url) 
-  
+    console.log("reached", wordSearch.value, url)
+
     fetch(url).then(response => response.json()).then(json => {
         console.log(json)
         meaning = json[0].meanings[0].definitions[0].definition;
         example = json[0].meanings[0].definitions[0].example
         synonyms = json[0].meanings[0].definitions[0].synonyms[0]
         console.log(meaning)
-        outputDiv.innerText = "Meaning:\n" + meaning + "\n\nExample:\n" + example + "\n\nSynonyms:\n" + synonyms
+        inputDiv1.innerText = "Meaning:\n" + meaning // + "\n\nExample:\n" + example + "Synonyms:\n" + synonyms
+        inputDiv2.innerText = "Example:\n" + example
+        inputDiv3.innerText = "Synonyms:\n" + synonyms
     })
-    
+
 }
 searchButton.addEventListener("click", search)
