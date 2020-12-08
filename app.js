28ce2032ffcd1bdb1824655562510f625d853fdd
Bhavesh-Chadha <bhaveshchadha14@gmail.com>
2020-12-08 14:45:30 +0530

    basic structured working website

--- app.js
+++ app.js
@@ -6,16 +6,24 @@ a = "word"
 url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 // url1 = url + word //wordSearch.value
 
-function getTranslationURL(input) {
-    return url + input
-}
+// function getTranslationURL(input) {
+//     return url + input
+// }
 
 function search() {
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+    url = url + wordSearch.value
+
+    console.log("reached", wordSearch.value, url) 
+  
+    fetch(url).then(response => response.json()).then(json => {
+        console.log(json)
+        meaning = json[0].meanings[0].definitions[0].definition;
+        example = json[0].meanings[0].definitions[0].example
+        synonyms = json[0].meanings[0].definitions[0].synonyms[0]
+        console.log(meaning)
+        outputDiv.innerText = "Meaning:\n" + meaning + "\n\nExample:\n" + example + "\n\nSynonyms:\n" + synonyms
+    })
     
-    console.log("reached" , word, url + a, url + word)//getTranslationURL(word)) // url1 + word)
-    fetch(getTranslationURL(word)).then(response => response.json()).then(json => console.log(json))
-    // .then(json=>outputDiv.innerText =json.0.meanings.0.definitions.0)
-    // .0.meanings.0.definitions.0))
-    // outputBox.innerText =
 }
 searchButton.addEventListener("click", search)
---
+++ tezt.js
@@ -0,0 +1,3 @@
+a=[b=[c=[d=[2,4],23],2,3]]
+console.log(a[0][0][0])
+// console.log(temp1[0].meanings[0].definitions[0].definition)
